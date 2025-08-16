f79b8be21482e5d837364b5b5f187562139c5184
grandbig <takahiro.kato15@gmail.com>
2025-08-17 00:31:39 +0900

    first commit

---
+++ components/ui/gluestack-ui-provider/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { config } from './config';
+import { View, ViewProps } from 'react-native';
+import { OverlayProvider } from '@gluestack-ui/overlay';
+import { ToastProvider } from '@gluestack-ui/toast';
+
+export type ModeType = 'light' | 'dark';
+
+export function GluestackUIProvider({
+  mode = 'light',
+  ...props
+}: {
+  mode?: ModeType;
+  children?: React.ReactNode;
+  style?: ViewProps['style'];
+}) {
+  return (
+    <View
+      style={[
+        config[mode],
+        { flex: 1, height: '100%', width: '100%' },
+        props.style,
+      ]}
+    >
+      <OverlayProvider>
+        <ToastProvider>{props.children}</ToastProvider>
+      </OverlayProvider>
+    </View>
+  );
+}
