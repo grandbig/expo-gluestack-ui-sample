8be69ef4e2fefef9ffe3d31bd191b9461ce84ceb
grandbig <takahiro.kato15@gmail.com>
2025-08-17 13:49:47 +0900

    src配下にcomponents, constants, hooksを移動しました。

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,10 @@ module.exports = {
   darkMode: 'media',
   content: [
     './app/**/*.{js,jsx,ts,tsx}',
-    './components/**/*.{js,jsx,ts,tsx}',
-    './hooks/**/*.{js,jsx,ts,tsx}',
+    './src/**/*.{html,js,jsx,ts,tsx}',
+    './src/core-components/**/**/*.{html,js,jsx,ts,tsx}',
+    './src/components/**/*.{html,js,jsx,ts,tsx,mdx}',
+    './src/hooks/**/*.{html,js,jsx,ts,tsx,mdx}',
   ],
   presets: [require('nativewind/preset')],
   safelist: [
